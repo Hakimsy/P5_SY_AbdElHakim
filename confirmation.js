cae7222363c1f743df6fcafffb1afc0a0aade0e4
Hakimsy <hakimsy17@gmail.com>
2021-04-15 06:18:30 +0000

    Adding last functionnality

--- confirmation.js
+++ confirmation.js
@@ -2,3 +2,4 @@ const contact = JSON.parse(localStorage.getItem("contact"));
 const orderId = JSON.parse(localStorage.getItem("orderId"));
 const total = JSON.parse(localStorage.getItem("total"));
 merci.innerText = `Merci ${contact.firstName} ! Voici votre numéro de commande : ${orderId}, pour un montant total de : ${total} €. `;
+localStorage.clear();
